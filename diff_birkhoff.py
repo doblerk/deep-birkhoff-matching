ee3eb6ccbc64410d7f503a6fe3054a371668ae4b
doblerk <kalvindobler@hotmail.com>
2025-04-01 16:40:50 +0200

    improved framework

--- diff_birkhoff.py
+++ diff_birkhoff.py
@@ -3,7 +3,10 @@ import torch
 import torch.nn as nn
 
 
-
+def compute_cost_matrix(representations1, representations2):
+  cost_matrix = torch.cdist(representations1, representations2, p=2)
+  # should we make it square? padding?
+  return cost_matrix
 
 
 
