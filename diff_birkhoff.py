0aa04c33e01fdcf4927b271d34507718252de845
doblerk <kalvindobler@hotmail.com>
2025-04-01 16:51:05 +0200

    added a permutation matrix generator

--- diff_birkhoff.py
+++ diff_birkhoff.py
@@ -8,7 +8,17 @@ def compute_cost_matrix(representations1, representations2):
   # should we make it square? padding?
   return cost_matrix
 
+def generate_permutation_matrices(N):
+    """
+    Generate all possible N x N permutation matrices.
+    """
+    perms = list(itertools.permutations(range(N)))
+    perm_matrices = torch.zeros(len(perms), N, N)
 
+    for i, perm in enumerate(perms):
+        for j, p in enumerate(perm):
+            perm_matrices[i, j, p] = 1
+    return perm_matrices  # Shape: (N!, N, N)
 
 class ContrastiveLoss(nn.Module):
 
