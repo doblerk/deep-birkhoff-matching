f0a8602ee3fc0fbc24ee0bb9fa63e62840a56fda
Kalvin Dobler <kalvindobler@hotmail.com>
2025-04-08 12:15:12 +0200

    added triplet dataset + refactored code

--- data_aug.py
+++ data_aug.py
@@ -4,6 +4,57 @@ import numpy as np
 from torch.utils.data import Dataset
 
 
+class TripletGraphDataset(Dataset):
+
+  def __init__(self, graphs, ged_labels, num_triplets=10000):
+    self.graphs = graphs
+    self.ged_labels = ged_labels
+    self.num_triplets = num_triplets
+    self.triplets = self._generate_triplets()
+  
+  def _generate_triplets(self):
+    triplets = []
+    num_graphs = len(self.graphs)
+
+    for _ in range(self.num_triplets):
+      anchor_idx = torch.randint(0, num_graphs - 1)
+      anchor_graph = self.graphs[anchor_idx]
+
+      # Rank all other graphs by GED
+      candidates = [
+        (i, self._get_ged(anchor_idx, i))
+        for i in range(num_graphs) if i != anchor_idx
+      ]
+      candidates.sort(key=lambda x: x[1])
+
+      # Find most similar and dissimlar graphs
+      pos_graph_idx = candidates[0][0]
+      neg_graph_idx = candidates[-1][0]
+
+      pos_graph = self.graphs[pos_graph_idx]
+      neg_graph = self.graphs[neg_graph_idx]
+
+      # sample random node idx
+      a_node = torch.randint(0, anchor_graph.num_nodes - 1)
+      p_node = torch.randint(0, pos_graph.num_nodes - 1)
+      n_node = torch.randint(0, neg_graph.num_nodes - 1)
+
+      triplets.append(((anchor_idx, a_node),
+                       (pos_graph_idx, p_node),
+                       neg_graph_idx, n_node))
+      
+    return triplets
+  
+  def _get_ged(self, i, j):
+    return self.ged_labels.get((i, j), self.ged_labels.get((j, i), 0.0))
+
+  def __len__(self):
+    return len(self.triplets)
+  
+  def __getitem__(self, idx):
+    return self.triplets[idx]
+
+
 class GraphPairDataset(Dataset):
 
   def __init__(self, graphs, ged_labels):
--- train_model.py
+++ train_model.py
@@ -27,33 +27,9 @@ def get_true_ged(batch1, batch2):
     return
 
 
-def main():
-
-    distance_matrix = np.load('./data/distances_alpha_-1.0.npy')
-
-    # Set device to CUDA
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    # Load the dataset from TUDataset
-    dataset = TUDataset(root='data', name='MUTAG')
-    ged_labels = get_ged_labels(distance_matrix)
-
-    # Prepare DataLoader
-    graph_pair_dataset = GraphPairDataset(dataset, ged_labels)
-
-    dataloader = DataLoader(graph_pair_dataset, batch_size=32, shuffle=True)
-
-    # Model, optimizer, and loss
-    model = Model(dataset.num_features, 64, 3).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.00001, weight_decay=0.0001)
-    criterion = ContrastiveLoss()
-
-    for epoch in range(1):
-        
+def train(train_loader, device, optimizer, model, criterion):
         model.train()
-        
-        for batch in dataloader:
-            print('New Batch')
+        for batch in train_loader:
             batch1, batch2, ged_labels = batch
             batch1, batch2, ged_labels = batch1.to(device), batch2.to(device), ged_labels.to(device)
 
@@ -85,6 +61,72 @@ def main():
 
             loss.backward()
             optimizer.step()
+
+
+@torch.no_grad()
+def test(test_loader, device, model, criterion):
+    model.eval()
+    history = []
+    for batch in test_loader:
+        batch1, batch2, ged_labels = batch
+        batch1, batch2, ged_labels = batch1.to(device), batch2.to(device), ged_labels.to(device)
+        
+        node_representations_b1 = model(batch1.x, batch1.edge_index)
+        node_representations_b2 = model(batch2.x, batch2.edge_index)
+
+        dense_b1, mask1 = to_dense_batch(node_representations_b1, batch1.batch)
+        dense_b2, mask2 = to_dense_batch(node_representations_b2, batch2.batch)
+
+        cost_matrices = compute_cost_matrix(dense_b1, dense_b2)
+
+        padded_cost_matrices = pad_cost_matrix(cost_matrices)
+
+        B, N, N = padded_cost_matrices.shape
+        k_plus_one = N + 1
+
+        pm = PermutationMatrix()
+        perm_matrices = pm.generate_permutation_matrices(B, N, k_plus_one).to(device)
+
+        soft_assignment_matrices = pm(perm_matrices)
+
+        predicted_ged = criterion(padded_cost_matrices, soft_assignment_matrices)
+
+        true_ged = torch.sum(ged_labels)
+            
+        loss = torch.nn.functional.mse_loss(predicted_ged, true_ged)
+
+        history.append(loss)
+
+    return history
+
+
+def train_model(dataloader, device, optimizer, model, criterion):
+     for epoch in range(1):
+          train(dataloader, device, optimizer, model, criterion)
+
+
+def main():
+
+    distance_matrix = np.load('./data/distances_alpha_-1.0.npy')
+
+    # Set device to CUDA
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Load the dataset from TUDataset
+    dataset = TUDataset(root='data', name='MUTAG')
+    ged_labels = get_ged_labels(distance_matrix)
+
+    # Prepare DataLoader
+    graph_pair_dataset = GraphPairDataset(dataset, ged_labels)
+
+    dataloader = DataLoader(graph_pair_dataset, batch_size=32, shuffle=True)
+
+    # Model, optimizer, and loss
+    model = Model(dataset.num_features, 64, 3).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.00001, weight_decay=0.0001)
+    criterion = ContrastiveLoss()
+
+    train_model(dataloader, device, optimizer, model, criterion)        
 
 
 if __name__ == '__main__':
