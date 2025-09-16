799cfd2a7f72775a23a1db3aa58c59e2cb513f42
doblerk <kalvindobler@hotmail.com>
2025-09-16 16:12:28 +0200

    added pmx method

--- tribiged/models/alpha_layers.py
+++ tribiged/models/alpha_layers.py
@@ -2,12 +2,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from typing import Tuple
 from tribiged.utils.permutation import PermutationPool
 
 
 class AlphaPermutationLayer(nn.Module):
    
-    def __init__(self, perm_matrices: torch.Tensor, k: int, embedding_dim: int):
+    def __init__(self, perm_matrices: torch.Tensor, k: int, embedding_dim: int, window_history: int = 20):
         super(AlphaPermutationLayer, self).__init__()
         self.perm_matrices = perm_matrices
         self.k = k
@@ -17,29 +18,34 @@ class AlphaPermutationLayer(nn.Module):
         self.alpha_mlp = nn.Sequential(
             nn.Dropout(0.4),
             nn.Linear(dim, dim * 2),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.LayerNorm(dim * 2),
             nn.Dropout(0.4),
             nn.Linear(dim * 2, dim * 2),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Linear(dim * 2, self.k)
         )
 
-        self.alpha_history = torch.zeros(self.k)
+        self.window_history = window_history
+        self.register_buffer("alpha_history", torch.zeros(self.k))
 
-    def get_alpha_weights(self):
-        return torch.softmax(self.alpha_logits / self.temperature, dim=1)
+    def get_alpha_weights(self, alpha_logits: torch.Tensor) -> torch.Tensor:
+        return F.softmax(alpha_logits / self.temperature, dim=1)
     
-    def _update_alpha_history(self, alphas):
-        return self.alpha_history + alphas
+    def update_alpha_history(self, alphas: torch.Tensor):
+        self.alpha_history += alphas.detach()
     
-    def _clear_alpha_history(self):
-        self.alpha_history[:] = 0.0
+    def clear_alpha_history(self):
+        self.alpha_history.zero_()
     
-    def _rank_alpha(self):
-        pass
+    def rank_alpha(self, k: int = 2) -> Tuple[torch.Tensor, torch.Tensor]:
+        mean_weights = self.alpha_history / self.window_history
+        sorted_idx = torch.argsort(mean_weights) # ascending
+        worst_idx = sorted_idx[:k]
+        best_idx = sorted_idx[-k:]
+        return worst_idx, best_idx
         
-    def forward(self, graph_repr_b1, graph_repr_b2):
+    def forward(self, graph_repr_b1: torch.Tensor, graph_repr_b2: torch.Tensor):
         pair_repr = torch.cat([graph_repr_b1, graph_repr_b2], dim=1)
         alpha_logits = self.alpha_mlp(pair_repr)
         alphas = F.softmax(alpha_logits / self.temperature, dim=1)
--- tribiged/utils/permutation.py
+++ tribiged/utils/permutation.py
@@ -2,6 +2,8 @@ import numpy as np
 
 import torch
 
+from typing import Tuple
+
 
 class PermutationPool:
     def __init__(self, max_n: int, k: int, seed: int = 42):
@@ -12,32 +14,36 @@ class PermutationPool:
             size_data (np.ndarray): Array of shape (N, 2) containing historical (n, m) size pairs
             seed (int): RNG seed for reproducibility
         """
-        self.rng = np.random.default_rng(seed)
+        self.rng = torch.Generator().manual_seed(seed) #np.random.default_rng(seed)
         self.max_n = max_n
         self.k = k
         self.perm_vectors = self._generate_permutation_vectors()
 
-    def _generate_permutation_vectors(self):
+    def _generate_permutation_vectors(self) -> torch.Tensor:
         """Generate permutation vectors of max_n length, padded/embedded."""
-        perms = []
-
-        identity = np.arange(self.max_n)
-        perms.append(tuple(identity))
-
-        for _ in range(self.k - 1):
-            perms.append(tuple(self.rng.permutation(self.max_n)))
-
-        return torch.tensor(perms, dtype=torch.long)
+        # perms = [np.arange(self.max_n)]
+        # for _ in range(self.k - 1):
+        #     perms.append(self.rng.permutation(self.max_n))
+        # return torch.tensor(perms, dtype=torch.long)
+        perms = torch.zeros((self.k, self.max_n))
+        perms[0] = torch.arange(0, self.max_n)
+        for i in range(1, self.k):
+            perms[i] = torch.randperm(self.max_n, generator=self.rng)
+        perms = perms.to("cuda")
+        return perms
     
-    def get_vectors(self):
+    def get_vectors(self) -> torch.Tensor:
         return self.perm_vectors
     
-    def _mate_permutations(self, alpha_weights):
-        ranking = torch.argsort(alpha_weights)
-        topk = alpha_weights[ranking][:2]
-        pass
-
-    def _partially_mapped_crossover(self, p1, p2):
+    def _resolve(self, value: int, seg_from: torch.Tensor, seg_to: torch.Tensor) -> int:
+        """Recursively resolve conflicts for a single value."""
+        if value in seg_to:
+            pos = (seg_to == value).nonzero(as_tuple=True)[0].item()
+            return self._resolve(seg_from[pos], seg_from, seg_to)
+        else:
+            return value
+    
+    def _partially_mapped_crossover(self, p1: torch.Tensor, p2: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         size = len(p1)
         c1, c2 = p1.clone(), p2.clone()
 
@@ -46,42 +52,35 @@ class PermutationPool:
         bp2 = torch.randint(bp1 + 1, size, (1,)).item()
 
         # Crossover segments
-        seg1 = p1[bp1:bp2]
-        seg2 = p2[bp1:bp2]
-
-        # Fill up mapping
-        self._fill_child(c1, seg1, seg2, size)
-        self._fill_child(c2, seg2, seg1, size)
+        seg1 = p1[bp1:bp2 + 1]
+        seg2 = p2[bp1:bp2 + 1]
 
         # Exchange segments
         c1[bp1:bp2 + 1] = seg2
         c2[bp1:bp2 + 1] = seg1
 
+        # Fill up mapping outside the crossover region
+        for i in list(range(0, bp1)) + list(range(bp2 + 1, size)):
+            c1[i] = self._resolve(c1[i].item(), seg1, seg2)
+            c2[i] = self._resolve(c2[i].item(), seg2, seg1)
+
         return c1, c2
     
-    def _resolve(self, value, seg_from, seg_to):
-        """Recursively resolve conflicts for a single value."""
-        if value in seg_to:
-            pos = (seg_to == value).nonzero(as_tuple=True)[0].item()
-            return self._resolve(seg_from[pos], seg_from, seg_to)
-        else:
-            return value
-
-    def _fill_child(self, c, seg_from, seg_to, size, idx=0):
-        if idx >= size:
-            return c
-        # If current gene is in the conflicting segment, resolve recursively
-        c[idx] = self._resolve(c[idx], seg_from, seg_to)
-        return self._fill_child(c, seg_from, seg_to, size, idx + 1)
+    def _mate_permutations(self, worst_idx: torch.Tensor, best_idx: torch.Tensor) -> None:
+        best1, best2 = self.perm_vectors[best_idx[0]], self.perm_vectors[best_idx[1]]
+        c1, c2 = self._partially_mapped_crossover(best1, best2)
+        self.perm_vectors[worst_idx[0]] = c1
+        self.perm_vectors[worst_idx[1]] = c2
     
     def get_matrix_batch(self):
         """
         Returns a batch of k permutation matrices of shape (k, max_n, max_n)
         One-hot encoded, inactive rows filled with zeros
         """
-        matrices = torch.zeros((self.k, self.max_n, self.max_n))
-        for idx, vec in enumerate(self.perm_vectors):
-            for i, j in enumerate(vec):
-                if j != -1:
-                    matrices[idx, i, j] = 1.0
+        matrices = torch.zeros((self.k, self.max_n, self.max_n), dtype=torch.long, device="cuda")
+        # for idx, vec in enumerate(self.perm_vectors):
+        #     for i, j in enumerate(vec):
+        #         if j != -1:
+        #             matrices[idx, i, j] = 1.0
+        matrices.scatter_(2, self.perm_vectors.unsqueeze(-1), 1.0)
         return matrices
